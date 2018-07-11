d750072c8b6fc469fc3a18d047a5997210c7a62c
John Wass <jwass3@gmail.com>
2018-07-11 11:56:37 -0400

    add a finder as the config entrypoint

---
+++ packages/common/cmake/FindParticle.cmake
@@ -0,0 +1,17 @@
+if (NOT PLATFORM)
+    message(WARNING "defaulting to photon platform, specify with PLATFORM")
+    set(PLATFORM photon)
+elseif (NOT PLATFORM MATCHES "^photon$|^electron$")
+    message(FATAL_ERROR "invalid platform [${PLATFORM}] (choose 'photon' or 'electron')")
+endif ()
+
+if (NOT FIRMWARE_VERSION)
+    message(FATAL_ERROR "Firmware version was not specified with FIRMWARE_VERSION")
+endif ()
+
+set(BUILD_DIR ${CMAKE_BINARY_DIR})
+set(GCC_ARM_PATH /usr/local/gcc-arm/bin/)
+set(FIRMWARE_DIR /usr/local/src/particle/firmware)
+
+include(${PLATFORM})
+include(particle)

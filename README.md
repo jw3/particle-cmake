76637cd80b79143a9ddaaee40d793ed9cb99e5d1
John Wass <jwass3@gmail.com>
2018-08-23 22:23:09 -0400

    publishing support

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Particle and CMake with Conan
 ===
 
 This is a collection of Conan packages providing the build configuration to support Particle firmware development with CMake.
+
+Packages are published to https://bintray.com/jw3/particle
--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ readonly builddir="${BUILD_DIR:-${sourcedir}/build}"
 
 readonly conanpath="${CONAN_PATH:-${sourcedir}}"
 readonly conanuser="${CONAN_USER:-jw3}"
-readonly conanchannel="${CONAN_CHANNEL:-particle}"
+readonly conanchannel="${CONAN_CHANNEL:-stable}"
 
 _echoerr() { if [[ ${QUIET} -ne 1 ]]; then echo "[debug]: $@" 1>&2; fi }
 _checkerr() { ec=${1}; if [[ ${ec} -ne 0 ]]; then _echoerr "$2 failed with error code $ec"; exit ${ec}; fi }
@@ -42,10 +42,17 @@ publish() {
   cmake ${sourcedir}
 
   version="${CONAN_VERSION:-$(cat ${builddir}/VERSION)}"
-  remote="${CONAN_REMOTE:-conan-center}"
+  remote="${CONAN_REMOTE:-particle-bintray}"
 
   _echoerr "conan upload $conanuser/$conanchannel"
-  conan upload "$conanuser/$conanchannel" -c -r "$remote" --all
+  conan upload "particle-common/$version@$conanuser/$conanchannel" -c -r "$remote" --all
+  conan upload "OneWire/$version@$conanuser/$conanchannel" -c -r "$remote" --all
+  conan upload "DS18B20/$version@$conanuser/$conanchannel" -c -r "$remote" --all
+  conan upload "TinyGpsPlus/$version@$conanuser/$conanchannel" -c -r "$remote" --all
+  conan upload "LiquidCrystalI2C/$version@$conanuser/$conanchannel" -c -r "$remote" --all
+  conan upload "AssetTrackerRK/$version@$conanuser/$conanchannel" -c -r "$remote" --all
+  conan upload "LIS3DH/$version@$conanuser/$conanchannel" -c -r "$remote" --all
+  conan upload "NeoGPS/$version@$conanuser/$conanchannel" -c -r "$remote" --all
 }
 
 

b73af49f8155d2bdcfe3b237510536fff632babc
John Wass <jwass3@gmail.com>
2019-10-07 15:19:12 -0400

    externalize dfu device path
    fixes #10

--- packages/common/cmake/flasher.cmake
+++ packages/common/cmake/flasher.cmake
@@ -1,2 +1,2 @@
-configure_file(${CONAN_PARTICLE-COMMON_ROOT}/bin/flash.in ${CMAKE_BINARY_DIR}/flash)
+configure_file(${CONAN_PARTICLE-COMMON_ROOT}/bin/flash.in ${CMAKE_BINARY_DIR}/flash @ONLY)
 configure_file(${CONAN_PARTICLE-COMMON_ROOT}/share/flash.mk.in ${CMAKE_BINARY_DIR}/${name}/flash.mk)

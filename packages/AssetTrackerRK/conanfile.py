57dd0a1b8322361e5c65f8e6a5179928950ccdc6
John Wass <jwass3@gmail.com>
2019-10-11 06:46:42 -0400

    convert asset tracker and lis3dh

--- packages/AssetTrackerRK/conanfile.py
+++ packages/AssetTrackerRK/conanfile.py
@@ -1,22 +1,34 @@
 from conans import ConanFile, tools
 import os
 
+repo_url = 'https://github.com/rickkas7/AssetTrackerRK.git'
+commit_sha='b913c5ac4967e83c466d8f079857ef349f8e7f69'
 build_dir = os.getenv("BUILD_DIR", "build")
+source_dir = os.getenv("PKG_SRC_DIR", None)
 
-def lookup(name, default = None):
+
+def lookup(name, default=None):
     v = default
     if os.path.exists(build_dir + "/" + name):
         v = tools.load(build_dir + "/" + name)
     return os.getenv("CONAN_" + name, v)
 
+
 class ParticlePackage(ConanFile):
     name = "AssetTrackerRK"
     version = lookup("VERSION", "snapshot")
     settings = "os", "compiler", "build_type", "arch"
-    url = "https://github.com/jw3/particle-cmakes"
+    url = "https://github.com/jw3/conan-particle"
     exports = build_dir + "/VERSION"
     generators = "cmake"
-    requires = []
+    requires = ['LIS3DH/9.9.9.9@jw3/stable']
 
     def package(self):
-        self.copy("*.cmake", src="cmake")
+        self.copy("*.cmake", src=f"{source_dir}/cmake")
+        self.copy("*.c*", dst="src", src='src')
+        self.copy("*.h*", dst="include", src='src')
+
+    def source(self):
+        self.run(f"git clone {repo_url} .")
+        if commit_sha:
+            self.run(f"git checkout {commit_sha}")
